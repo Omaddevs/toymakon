550fa55b3f2262c3a987f847fd57ccea0778d0f2
Omadbek Egamberdiyev <omaddevs@gmail.com>
2026-04-18 14:42:37 +0500

    Fix mobile browser padding compatibility

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
       </div>
 
       {/* Categories Row */}
-      <section className="home-section pad-x">
+      <section className="home-section">
         <div className="horizontal-scroll hide-scrollbar">
           {[
             { icon: "ph-buildings", label: "To‘yxonalar" },
